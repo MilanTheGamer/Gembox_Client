2296f840326b836d46921a509aca85d43cd7e666
Typical Engineer <milanthegamer@gmail.com>
2022-06-17 21:51:17 +0400

    typography updated

--- components/Typography/index.tsx
+++ components/Typography/index.tsx
@@ -2,12 +2,32 @@ import styled from "styled-components";
 
 export interface TypographyInterface {
   type?: string;
+  variant?: "onSurface" | "onPrimary" | "primary";
+  emphasis?: "high" | "medium" | "disabled";
   children: string;
 }
 
 // --------------------- TITLE ---------------------------------
 
 const TitleWrapper = styled.div`
+  &.title_onSurface {
+    color: ${({ theme }) => theme.onSurface};
+  }
+  &.title_onPrimary {
+    color: ${({ theme }) => theme.onPrimary};
+  }
+  &.title_primary {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.title_high_emphasis {
+    opacity: 0.87;
+  }
+  &.title_medium_emphasis {
+    opacity: 0.6;
+  }
+  &.title_disabled_emphasis {
+    opacity: 0.38;
+  }
   h1 {
     font-size: ${({ theme }) => theme.fontSize.h1};
     font-weight: ${({ theme }) => theme.fontWeight.bold};
@@ -53,13 +73,42 @@ const switchTitle = (type: string, children: any) => {
   }
 };
 
-export const Title = ({ type = "H1", children }: TypographyInterface) => {
-  return <TitleWrapper>{switchTitle(type, children)}</TitleWrapper>;
+export const Title = ({
+  type = "H1",
+  variant = "onSurface",
+  emphasis = "high",
+  children,
+}: TypographyInterface) => {
+  return (
+    <TitleWrapper
+      className={`typography typography_title title_${variant} title_${emphasis}_emphasis`}
+    >
+      {switchTitle(type, children)}
+    </TitleWrapper>
+  );
 };
 
 // ------------------- SUBTITILE ------------------------------------
 
 const SubtitleWrapper = styled.div`
+  &.subtitle_onSurface {
+    color: ${({ theme }) => theme.onSurface};
+  }
+  &.subtitle_onPrimary {
+    color: ${({ theme }) => theme.onPrimary};
+  }
+  &.subtitle_primary {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.subtitle_high_emphasis {
+    opacity: 0.87;
+  }
+  &.subtitle_medium_emphasis {
+    opacity: 0.6;
+  }
+  &.subtitle_disabled_emphasis {
+    opacity: 0.38;
+  }
   .subtitle1 {
     font-size: ${({ theme }) => theme.fontSize.sub1};
     font-weight: ${({ theme }) => theme.fontWeight.regular};
@@ -81,13 +130,42 @@ const switchSubtitle = (type: string, children: any) => {
   }
 };
 
-export const Subtitle = ({ type = "S1", children }: TypographyInterface) => {
-  return <SubtitleWrapper>{switchSubtitle(type, children)}</SubtitleWrapper>;
+export const Subtitle = ({
+  type = "S1",
+  variant = "onSurface",
+  emphasis = "high",
+  children,
+}: TypographyInterface) => {
+  return (
+    <SubtitleWrapper
+      className={`typography typography_subtitle subtitle_${variant} subtitle_${emphasis}_emphasis`}
+    >
+      {switchSubtitle(type, children)}
+    </SubtitleWrapper>
+  );
 };
 
 // -------------------- TEXT ---------------------------------------------
 
 const TextWrapper = styled.div`
+  &.text_onSurface {
+    color: ${({ theme }) => theme.onSurface};
+  }
+  &.text_onPrimary {
+    color: ${({ theme }) => theme.onPrimary};
+  }
+  &.text_primary {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.text_high_emphasis {
+    opacity: 0.87;
+  }
+  &.text_medium_emphasis {
+    opacity: 0.6;
+  }
+  &.text_disabled_emphasis {
+    opacity: 0.38;
+  }
   .body1 {
     font-size: ${({ theme }) => theme.fontSize.body1};
     font-weight: ${({ theme }) => theme.fontWeight.regular};
@@ -109,28 +187,95 @@ const switchText = (type: string, children: any) => {
   }
 };
 
-export const Text = ({ type = "B1", children }: TypographyInterface) => {
-  return <TextWrapper>{switchText(type, children)}</TextWrapper>;
+export const Text = ({
+  type = "B1",
+  variant = "onSurface",
+  emphasis = "high",
+  children,
+}: TypographyInterface) => {
+  return (
+    <TextWrapper
+      className={`typography typography_text text_${variant} text_${emphasis}_emphasis`}
+    >
+      {switchText(type, children)}
+    </TextWrapper>
+  );
 };
 
 // ------------------- CAPTION -------------------------------------------
 
 const CaptionWrapper = styled.div`
+  &.caption_onSurface {
+    color: ${({ theme }) => theme.onSurface};
+  }
+  &.caption_onPrimary {
+    color: ${({ theme }) => theme.onPrimary};
+  }
+  &.caption_primary {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.caption_high_emphasis {
+    opacity: 0.87;
+  }
+  &.caption_medium_emphasis {
+    opacity: 0.6;
+  }
+  &.caption_disabled_emphasis {
+    opacity: 0.38;
+  }
   font-size: ${({ theme }) => theme.fontSize.caption};
   font-weight: ${({ theme }) => theme.fontWeight.regular};
 `;
 
-export const Caption = ({ children }: TypographyInterface) => {
-  return <CaptionWrapper>{children}</CaptionWrapper>;
+export const Caption = ({
+  variant = "onSurface",
+  emphasis = "high",
+  children,
+}: TypographyInterface) => {
+  return (
+    <CaptionWrapper
+      className={`typography typography_caption caption_${variant} caption_${emphasis}_emphasis`}
+    >
+      {children}
+    </CaptionWrapper>
+  );
 };
 
 // ------------------ OVERLINE ------------------------------------------
 
 const OverlineWrapper = styled.div`
+  &.overline_onSurface {
+    color: ${({ theme }) => theme.onSurface};
+  }
+  &.overline_onPrimary {
+    color: ${({ theme }) => theme.onPrimary};
+  }
+  &.overline_primary {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.overline_high_emphasis {
+    opacity: 0.87;
+  }
+  &.overline_medium_emphasis {
+    opacity: 0.6;
+  }
+  &.overline_disabled_emphasis {
+    opacity: 0.38;
+  }
   font-size: ${({ theme }) => theme.fontSize.overline};
   font-weight: ${({ theme }) => theme.fontWeight.regular};
 `;
 
-export const Overline = ({ children }: TypographyInterface) => {
-  return <OverlineWrapper>{children.toUpperCase()}</OverlineWrapper>;
+export const Overline = ({
+  variant = "onSurface",
+  emphasis = "high",
+  children,
+}: TypographyInterface) => {
+  return (
+    <OverlineWrapper
+      className={`typography typography_overline overline_${variant} overline_${emphasis}_emphasis`}
+    >
+      {children.toUpperCase()}
+    </OverlineWrapper>
+  );
 };
