e30b73b16d94a0ccff03e539a1ae0a7ae07c7fc3
Typical Engineer <milanthegamer@gmail.com>
2022-07-02 21:03:49 +0400

    hotlist added

---
+++ components/HotList/index.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import Card from "../Card";
+
+const testData: any[] = [
+  {
+    id: 1,
+    type: "24h",
+    blockchain: "etherium",
+    bidAmount: 10,
+    priority: "high",
+  },
+  {
+    id: 2,
+    type: "24h",
+    blockchain: "etherium",
+    bidAmount: 10,
+    priority: "low",
+  },
+  {
+    id: 3,
+    type: "7h",
+    blockchain: "etherium",
+    bidAmount: 10,
+    priority: "medium",
+  },
+];
+
+const cardTypeResolver = (priority: string) => {
+  switch (priority) {
+    case "high":
+      return "big";
+    case "medium":
+      return "long";
+    default:
+      return "small";
+  }
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 80px);
+  grid-template-rows: repeat(6, 80px);
+  grid-gap: 20px;
+`;
+
+const HotList = () => {
+  return (
+    <Wrapper>
+      {testData.map((item) => {
+        return (
+          <Card
+            key={item.id}
+            data={item}
+            type={cardTypeResolver(item.priority)}
+          />
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+export default HotList;
